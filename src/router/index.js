d67bc7203d8fd367a34de3adad3160676e7a1a22
袁俊辉 <yjh930304@163.com>
2026-02-04 17:10:49 +0800

    熟悉路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+// createRouter 创建路由实例
+// 配置 history 模式
+// 1. history模式：createWebHistory   地址栏不带#
+// 2. hash模式：createWebHashHistory  地址栏带#
+
+// vite 中的环境变量 import.meta.env.BASE_URL 就是 vite.config.js 中的 base 配置项
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  // history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: []
 })
 
 export default router
