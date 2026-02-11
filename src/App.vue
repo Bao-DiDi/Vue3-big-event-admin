dcab2edf4920a69a0f37ee0fc0af7baea0d37675
袁俊辉 <yjh930304@163.com>
2026-02-11 18:09:22 +0800

    搭建登录页面、完成注册数据校验、接口调用、elementUI部分组件使用

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,5 @@
-<script setup>
-import { useRoute, useRouter } from 'vue-router'
-import { useUserStore, useCountStore } from './stores'
-
-// 获取路由对象
-const router = useRouter()
-// 获取路由参数
-const route = useRoute()
-
-const goList = () => {
-  console.log(router)
-  console.log(route)
-  router.push('/list')
-}
-
-const userStore = useUserStore()
-const countStore = useCountStore()
-</script>
+<script setup></script>
 <template>
-  <hr />
   <router-view></router-view>
-  <hr />
-  <hr />
-  <hr />
-  <div>我是APP</div>
-  <button @click="$router.push('/home')">跳首页</button>
-  <button @click="goList">跳列表页</button>
-  <div>
-    <p>{{ userStore.token }}</p>
-    <el-button type="primary" @click="userStore.setToken('slkdfljfelfnfnlenfie')">登录</el-button>
-    <el-button @click="userStore.removeToken">退出</el-button>
-  </div>
-  <div>
-    <p>{{ countStore.count }}</p>
-    <el-button @click="countStore.add(1)">加法 +1</el-button>
-  </div>
 </template>
 <style scoped></style>
