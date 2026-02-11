cc113da4030d079a28f1599b21ab5a3925022fe1
袁俊辉 <yjh930304@163.com>
2026-02-11 10:27:40 +0800

    导入elementPlus UI库，配置按需导入

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,12 @@ const goList = () => {
   <div>我是APP</div>
   <button @click="$router.push('/home')">跳首页</button>
   <button @click="goList">跳列表页</button>
+  <div>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 <style scoped></style>
