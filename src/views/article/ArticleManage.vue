77fd07f1096d9cee65a9223e21def5346f695938
袁俊辉 <yjh930304@163.com>
2026-03-23 15:49:14 +0800

    文章管理基本结构搭建

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -1,9 +1,92 @@
+<script setup>
+import { ref } from 'vue'
+import { Edit, Delete } from '@element-plus/icons-vue'
+
+const articleList = ref([
+  {
+    id: 5961,
+    title: '新的文章啊',
+    pub_date: '2022-07-10 14:53:52.604',
+    state: '已发布',
+    cate_name: '体育'
+  },
+  {
+    id: 5962,
+    title: '新的文章啊',
+    pub_date: '2022-07-10 14:54:30.904',
+    state: '草稿',
+    cate_name: '体育'
+  }
+])
+// 编辑文章
+const onEditArticle = (row) => {
+  console.log('编辑', row)
+}
+// 删除文章
+const onDeleteArticle = (row) => {
+  console.log('删除', row)
+}
+</script>
+
 <template>
   <page-container title="文章管理">
     <template #extra>
       <el-button type="primary">添加文章</el-button>
     </template>
-    主题部分，是表格 + 分页
+
+    <!-- 表单区域 -->
+    <el-form inline>
+      <el-form-item label="文章分类:">
+        <!-- label 展示给用户看的， value 提交给后台的 -->
+        <el-select>
+          <el-option label="新闻" value="110"></el-option>
+          <el-option label="体育" value="137"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="发布分类:">
+        <!-- 这里后台标记发布状态，是通过中文标记的，已发布 / 草稿 -->
+        <el-select>
+          <el-option label="已发布" value="已发布"></el-option>
+          <el-option label="草稿" value="草稿"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">搜索</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <!-- 表格区域 -->
+    <el-table :data="articleList">
+      <el-table-column label="文章标题" prop="title">
+        <template #default="{ row }">
+          <!-- underline 下划线 -->
+          <el-link :underline="'never'" type="primary">{{ row.title }}</el-link>
+        </template>
+      </el-table-column>
+      <el-table-column label="分类" prop="cate_name"></el-table-column>
+      <el-table-column label="发表时间" prop="pub_date"></el-table-column>
+      <el-table-column label="状态" prop="state"></el-table-column>
+      <!-- 利用作用域插槽 row 可以获取当前行的数据 => v-for 遍历 item -->
+      <el-table-column label="操作">
+        <template #default="{ row }">
+          <el-button
+            circle
+            plain
+            type="primary"
+            :icon="Edit"
+            @click="onEditArticle(row)"
+          ></el-button>
+          <el-button
+            circle
+            plain
+            type="danger"
+            :icon="Delete"
+            @click="onDeleteArticle(row)"
+          ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </page-container>
 </template>
 <style lang="scss" scoped></style>
